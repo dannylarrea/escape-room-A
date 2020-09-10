632582af8d6654adcc1c4a1ce0ff66c89ad8c5e7
dannylarrea <dannyjla15@gmail.com>
2020-09-10 21:55:13 +0200

    Update codi.js

--- codi.js
+++ codi.js
@@ -2,7 +2,7 @@ function validarform(){
     if(document.getElementById("A").value=="1,62" && document.getElementById("B").value=="(1+sqrt(5))/2"){
         return true;
     }else{
-        alert("Nop, vamos inténtalo de nuevo!")
+        alert("Inténtalo de nuevo!")
         return false;
     }
 }
