1e3192da2217575bad0ce504eec2443902b9aa31
Danny <dannyjla15@gmail.com>
2020-09-10 20:28:00 +0200

    2

--- codi.js
+++ codi.js
@@ -1,5 +1,4 @@
 function validarform(){
-    alert('hey')
     if(document.getElementById("A").value=="1,62" && document.getElementById("B").value=="(1+sqrt(5))/2"){
         return true;
     }else{
