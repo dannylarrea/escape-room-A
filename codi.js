e9f81276ca18413bc75edc7b503ce6f21a7b5b4c
Danny Larrea <danny@MacBook-Air-de-Danny.local>
2020-10-09 08:09:05 +0200

    update (09/10/2020)

--- codi.js
+++ codi.js
@@ -1,8 +1,18 @@
-function validarform(){
-    if(document.getElementById("A").value=="1,62" && document.getElementById("B").value=="(1+sqrt(5))/2"){
-        return true;
-    }else{
-        alert("Intenta-ho de nou!")
-        return false;
+function validarform() {
+    try {
+        if (document.getElementById("A").value == "1,62" && document.getElementById("B").value == "(1+sqrt(5))/2") {
+            return true;
+        }
+        /*else if (condition) {
+               //code
+           }*/
+        else {
+            alert("Intenta-ho de nou!");
+            return false;
+        }
+    } catch (error) {
+        //alert(error);
     }
+    alert("Intenta-ho de nou!");
+    return false;
 }
