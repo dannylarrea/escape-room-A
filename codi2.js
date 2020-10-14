8a462f73b671cd4373c38a6bede303816950428a
carlosmirabal <100005846.joan23@fje.edu>
2020-10-14 17:49:11 +0200

    Final repte2

---
+++ codi2.js
@@ -0,0 +1,18 @@
+function validarform() {
+    try {
+        if (document.getElementById("A").value == "DAW2") {
+            return true;
+        }
+        /*else if (condition) {
+               //code
+           }*/
+        else {
+            alert("Intenta-ho de nou!");
+            return false;
+        }
+    } catch (error) {
+        //alert(error);
+    }
+    alert("Intenta-ho de nou!");
+    return false;
+}
